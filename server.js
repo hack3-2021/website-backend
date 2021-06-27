b7a6a0cd060885b71370023bf8eb63433799b396
Oscar <oscaz45@gmail.com>
2021-06-27 16:04:40 +1000

    /api/profile db integration

--- server.js
+++ server.js
@@ -15,8 +15,26 @@ connection.connect()
 
 app.get('/api/profile', async (req, res) => {
   email = req.query.email;
-  const rows = await connection.query('SELECT * FROM users WHERE email=' + connection.escape(email))
-  console.log(rows)
+  const rows = await connection.query('SELECT * FROM users WHERE email=' + connection.escape(email) + ';')
+  if (rows.length == 0) {
+  	res.sendStatus(404);
+  	return;
+  }
+  let row = rows[0];
+  let communityID = row.communityID;
+  const suburbResponse = await connection.query('SELECT suburb FROM communities WHERE communityID=' + connection.escape(communityID) + ';')
+  let suburb = suburbResponse[0].suburb;
+  
+  response = {
+  	"email": row.email,
+  	"firstName": row.firstName,
+  	"lastName": row.lastName,
+  	"picture": row.picture,
+  	"bio": row.bio,
+  	"phoneNumber": row.phoneNumber,
+  	"vaccinated": row.vaccinated,
+  	"suburb": suburb
+  }
 })
 
 app.listen(port, () => {
